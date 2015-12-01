9340112172d7aad30b317ed5d71bf8cd7afab745
Laszlo Meszaros <lacienator@gmail.com>
2015-12-01 20:12:43 +0100

    tests: made func tests a bit flexible
     * From now on the syslog-ng binary doesn't have to be relative to func
       tests. It can be passed via SYSLOG_NG_BINARY environment variable.

--- tests/functional/globals.py
+++ tests/functional/globals.py
@@ -1,13 +1,27 @@
 import os
 
+def get_module_dir():
+    module_path = os.popen("%s --version | grep \"Module-Directory:\" | cut -d ' ' -f 3" % get_syslog_ng_binary(), 'r').read().split(".")
+    return '%s:' % module_path
+
+def get_module_path():
+    module_path = get_module_dir()
+    for (root, dirs, files) in os.walk(os.path.abspath(os.path.join(os.environ['top_builddir'], 'modules'))):
+        module_path += ':'.join(map(lambda x: root + '/' + x + '/.libs', dirs))
+        break
+    return module_path
+
+def get_syslog_ng_binary():
+    return os.getenv('SYSLOG_NG_BINARY', '../../syslog-ng/syslog-ng')
+
 def is_premium():
-    version = os.popen('../../syslog-ng/syslog-ng -V', 'r').read()
+    version = os.popen('%s -V' % get_syslog_ng_binary(), 'r').read()
     if version.find('premium-edition') != -1:
         return True
     return False
 
 def has_module(module):
-    avail_mods = os.popen('../../syslog-ng/syslog-ng -V | grep ^Available-Modules: ', 'r').read()
+    avail_mods = os.popen('%s -V | grep ^Available-Modules: ' % get_syslog_ng_binary(), 'r').read()
     if avail_mods.find(module) != -1:
         return True
     return False
